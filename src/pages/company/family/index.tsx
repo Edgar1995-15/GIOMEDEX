2cf00f3d588d805ed7af7ae34db52b177a4d5b42
Edgar <edgar.khojayan@livevegas.live>
2024-03-31 23:39:10 +0400

    fixed the slides and scrolls to pages

--- src/pages/company/family/index.tsx
+++ src/pages/company/family/index.tsx
@@ -1,10 +1,10 @@
-import { FC, useState } from 'react';
+import { FC } from 'react';
 import Card from '../../../components/Card';
 import data from '../data.json';
 import Typography from '../../../components/Typography';
+import CarouselSlider from '../../../components/CarouselSlider';
 
 const Family: FC = () => {
-  const [activeTab, setActiveTab] = useState(1);
   return (
     <div id='family' className="m-auto mt-14 flex w-full max-w-[1440px] flex-col max-md:mt-9">
       <div className="max-w-[90%] pl-[133px] max-md:pl-[18px]">
@@ -25,43 +25,50 @@ const Family: FC = () => {
           color="text-[#595959]"
         />
       </div>
-      <div className="mt-9 flex justify-center gap-9 max-md:pb-9">
+      <div className="mt-9 flex justify-center gap-9 max-md:pb-9 max-md:hidden">
         <Card
           avatar="/images/avatars/paul.png"
           name="Michaelis, Paul"
           subtitle="Board Member, Leipzig, Germany."
           text="Leads the board in setting strategic goals and objectives to ensure the company's growth and success in the field of clinical data management, statistics, and bio- informatics."
-          className={`${activeTab === 1 ? '' : 'max-md:hidden'}`}
         />
         <Card
           avatar="/images/avatars/diego.png"
           name="González, Diego"
           subtitle="Board Member, Yerevan, Armenia."
           text="Contributes to board discussions and decision-making processes to shape the company's strategic direction and ensure its long-term viability and success in the field of clinical data management."
-          className={`mt-9 pb-14 ${activeTab === 2 ? '' : 'max-md:hidden'}`}
+          className={`mt-9 pb-14`}
         />
         <Card
           avatar="/images/avatars/armen.png"
           name="Stepanyan , Armen"
           subtitle="Chief Statistical Officer, Yerevan, Armenia."
           text="Oversees the statistical operations of the company, ensuring the integrity and accuracy of data analysis and reporting for clinical research projects."
-          className={`${activeTab === 3 ? '' : 'max-md:hidden'}`}
         />
       </div>
-      <div className="m-auto my-9 hidden gap-2 max-md:flex">
-        <div
-          className={`h-[14px] w-[14px] rounded-full border-[2px] border-white ${activeTab === 1 ? 'bg-[#009462]' : 'bg-[#E1E1E1]'}`}
-          onClick={() => setActiveTab(1)}
-        ></div>
-        <div
-          className={`h-[14px] w-[14px] rounded-full border-[2px] border-white ${activeTab === 2 ? 'bg-[#009462]' : 'bg-[#E1E1E1]'} bg-[#009462]`}
-          onClick={() => setActiveTab(2)}
-        ></div>
-        <div
-          className={`h-[14px] w-[14px] rounded-full border-[2px] border-white ${activeTab === 3 ? 'bg-[#009462]' : 'bg-[#E1E1E1]'} bg-[#009462]`}
-          onClick={() => setActiveTab(3)}
-        ></div>
-      </div>
+      <CarouselSlider className='mt-9 flex justify-center flex-col m-a max-md:pb-9'>
+      <Card
+          avatar="/images/avatars/paul.png"
+          name="Michaelis, Paul"
+          subtitle="Board Member, Leipzig, Germany."
+          className='m-auto'
+          text="Leads the board in setting strategic goals and objectives to ensure the company's growth and success in the field of clinical data management, statistics, and bio- informatics."
+        />
+        <Card
+          avatar="/images/avatars/diego.png"
+          name="González, Diego"
+          subtitle="Board Member, Yerevan, Armenia."
+          text="Contributes to board discussions and decision-making processes to shape the company's strategic direction and ensure its long-term viability and success in the field of clinical data management."
+          className={`mt-9 pb-14 m-auto`}
+        />
+        <Card
+          avatar="/images/avatars/armen.png"
+          name="Stepanyan , Armen"
+          subtitle="Chief Statistical Officer, Yerevan, Armenia."
+          className='m-auto'
+          text="Oversees the statistical operations of the company, ensuring the integrity and accuracy of data analysis and reporting for clinical research projects."
+        />
+      </CarouselSlider>
     </div>
   );
 };
--- src/pages/company/mission/index.tsx
+++ src/pages/company/mission/index.tsx
@@ -1,17 +1,17 @@
-import { FC, useState } from 'react';
+import { FC } from 'react';
 import Icon from '../../../components/Icon';
 import data from '../data.json';
 import Typography from '../../../components/Typography';
+import CarouselSlider from '../../../components/CarouselSlider';
 
 const Mission: FC = () => {
-  const [activeTab, setActiveTab] = useState<number>(1);
   return (
     <div
-      id='mission'
-      className={`flex min-h-[333px] w-full justify-center bg-gradient-to-br from-[#009462] to-[#3B579D] ${activeTab === 2 ? 'max-md:h-[500px]' : 'max-md:h-[434px]'}`}
+      id="mission"
+      className={`flex min-h-[333px] w-full justify-center bg-gradient-to-br from-[#009462] to-[#3B579D]`}
     >
       <div className="container mx-32 mb-9 mt-9 flex max-w-[1440px] justify-around gap-10 max-md:mx-4 max-md:max-w-[324px] max-md:flex-col max-md:justify-center max-md:gap-0">
-        <div className={`max-w-[526px] ${activeTab === 1 ? '' : 'max-md:hidden'}`}>
+        <div className={`max-w-[526px] max-md:hidden`}>
           <div className="flex items-center gap-2">
             <Icon name="points" />
             <Typography
@@ -25,9 +25,7 @@ const Mission: FC = () => {
             className="mt-4 text-justify leading-6 text-white"
           />
         </div>
-        <div
-          className={`max-w-[526px] ${activeTab === 2 ? '' : 'max-md:hidden'} max-md:max-w-full max-md:pr-0`}
-        >
+        <div className={`max-w-[526px] max-md:hidden max-md:max-w-full max-md:pr-0`}>
           <div className="flex items-center gap-2">
             <Icon name="points" />
             <Typography
@@ -41,16 +39,36 @@ const Mission: FC = () => {
             className="left-6 mt-4 text-justify text-white"
           />
         </div>
-        <div className="m-auto mt-14 hidden gap-2 max-md:flex">
-          <div
-            className={`h-[14px] w-[14px] rounded-full border-[2px] border-white ${activeTab === 1 ? 'bg-[#009462]' : 'bg-white'}`}
-            onClick={() => setActiveTab(1)}
-          ></div>
-          <div
-            className={`h-[14px] w-[14px] rounded-full border-[2px] border-white ${activeTab === 2 ? 'bg-[#009462]' : 'bg-white'} bg-[#009462]`}
-            onClick={() => setActiveTab(2)}
-          ></div>
-        </div>
+        <CarouselSlider>
+          <div className={`max-w-[526px]`}>
+            <div className="flex items-center gap-2">
+              <Icon name="points" />
+              <Typography
+                content={data.mission.title}
+                className="!text-[24px] text-white"
+                variant="font-bold"
+              />
+            </div>
+            <Typography
+              content={data.mission.content}
+              className="mt-4 text-justify leading-6 text-white"
+            />
+          </div>
+          <div className={`max-w-[526px] max-md:max-w-full max-md:pr-0`}>
+            <div className="flex items-center gap-2">
+              <Icon name="points" />
+              <Typography
+                content={data.vision.title}
+                className="!text-[24px] text-white"
+                variant="font-bold"
+              />
+            </div>
+            <Typography
+              content={data.vision.content}
+              className="left-6 mt-4 text-justify text-white"
+            />
+          </div>
+        </CarouselSlider>
       </div>
     </div>
   );
